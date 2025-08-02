20a8f8d6f911f4bfd5549cc1eb7fb6fb0222378d
avinashshelke3082 <avinashshelke3082@gmail.com>
2025-08-03 00:32:20 +0530

    add: 347. Top K Frequent Elements

---
+++ DSA/src/main/java/com/dsa/questions/potd/TopKFrequentElements.java
@@ -0,0 +1,42 @@
+package com.dsa.questions.potd;
+// Day - 31/07/25
+
+import java.util.*;
+
+public class TopKFrequentElements {
+    private record Pair(int first, int second) implements Comparable<Pair>{
+        @Override
+        public int compareTo(Pair o){
+            return Integer.compare(o.first,this.first);
+        }
+    }
+
+    public static int[] topKFrequent(int[] nums, int k){
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        Arrays.stream(nums).forEach((num)->{
+            Integer earlierFreq = mp.getOrDefault(num, 0);
+            mp.put(num, earlierFreq+1);
+        });
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        mp.forEach((key,value)->{
+            pq.add(new Pair(value, key));
+        });
+
+        int [] ans = new int[k];
+        for(int i=0;i<k;i++){
+            Pair p = pq.poll();
+            ans[i] = p.second;
+        }
+
+        return ans;
+    }
+
+    public static void main(String [] args){
+        int [] nums={1,1,1,2,2,3};
+        int k=2;
+        int [] ans = topKFrequent(nums, k);
+        System.out.println(Arrays.toString(ans));
+    }
+}
