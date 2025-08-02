afb51cbaba47af7f6337bfed4fa2913d9ab1d0a9
avinashshelke3082 <avinashshelke3082@gmail.com>
2025-08-03 00:35:25 +0530

    add: 36. Valid Sudoku

---
+++ DSA/src/main/java/com/dsa/questions/potd/ValidSudoku.java
@@ -0,0 +1,53 @@
+package com.dsa.questions.potd;
+// Day - 31/07/25
+
+import java.util.*;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        for(int i=0;i<board.length;i++){
+            Set<Character> st = new HashSet<>();
+            for(int j=0;j<board.length;j++){
+                if(board[i][j] != '.'){
+                    if(st.contains(board[i][j])){
+                        return false;
+                    }
+                    st.add(board[i][j]);
+                }
+            }
+
+        }
+        for(int i=0;i<board.length;i++){
+            Set<Character> st = new HashSet<>();
+            for(int j=0;j<board.length;j++){
+                if(board[j][i] != '.'){
+                    if(st.contains(board[j][i])){
+                        return false;
+                    }
+                    st.add(board[j][i]);
+                }
+            }
+
+        }
+
+        int i=0;
+        for(;i<board.length;i=i+3){
+            int l=0;
+            for(;l<board.length;l=l+3){
+                Set<Character> st = new HashSet<>();
+                for(int j=i;j<i+3;j++){
+                    for(int k=l;k<l+3;k++){
+                        if(board[j][k] != '.'){
+                            if(st.contains(board[j][k])){
+                                return false;
+                            }
+                            st.add(board[j][k]);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
