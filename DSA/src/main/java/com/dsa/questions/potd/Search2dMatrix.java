be33b1b1f29a18e7528064897fb85bd64bedaaf7
avinashshelke3082 <avinashshelke3082@gmail.com>
2025-08-04 21:15:17 +0530

    add: 74. Search a 2D Matrix

---
+++ DSA/src/main/java/com/dsa/questions/potd/Search2dMatrix.java
@@ -0,0 +1,32 @@
+package com.dsa.questions.potd;
+
+public class Search2dMatrix {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows*cols -1;
+
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            int val = matrix[mid/cols][mid%cols];
+
+            if(val == target){
+                return true;
+            } else if (val < target) {
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return false;
+
+    }
+    public static void main(String [] args){
+        int [][] matrix = new int [][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int target = 3;
+        boolean ans = searchMatrix(matrix, target);
+        System.out.println(ans);
+    }
+}
