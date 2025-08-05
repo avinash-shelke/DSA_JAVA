dead59980f5d0260d324ac18dcea1ac708dafe74
avinashshelke3082 <avinashshelke3082@gmail.com>
2025-08-05 22:06:53 +0530

    add: 42. Trapping Rain Water

---
+++ DSA/src/main/java/com/dsa/questions/potd/TrappingRainWater.java
@@ -0,0 +1,64 @@
+package com.dsa.questions.potd;
+// Day - 04/08/25
+
+public class TrappingRainWater {
+
+    public enum AffixType {
+        PREFIX,
+        SUFFIX
+    }
+
+    public static int[] calculateMax(int[] height, AffixType traverse) {
+        int n = height.length;
+        int[] arr = new int[n];
+        switch (traverse) {
+            case PREFIX -> {
+                int max = -1;
+                for (int i = 0; i < n; i++) {
+                    if (height[i] > max) {
+                        arr[i] = height[i];
+                        max = height[i];
+                    } else {
+                        arr[i] = max;
+                    }
+                }
+                return arr;
+            }
+            case SUFFIX -> {
+                int max = -1;
+                for (int i = n - 1; i >= 0; i--) {
+                    if (height[i] > max) {
+                        arr[i] = height[i];
+                        max = height[i];
+                    } else {
+                        arr[i] = max;
+                    }
+                }
+                return arr;
+            }
+        }
+        return null;
+    }
+
+    public static int trap(int[] height) {
+
+        int[] prefixMax = calculateMax(height, AffixType.PREFIX);
+        int[] suffixMax = calculateMax(height, AffixType.SUFFIX);
+
+        int n = height.length;
+        int ans=0;
+        for (int i = 1; i < n - 1; i++) {
+            int mini = StrictMath.min(prefixMax[i], suffixMax[i]);
+            ans += (mini - height[i]);
+        }
+
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        int[] height = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int ans = trap(height);
+        System.out.println(ans);
+    }
+}
